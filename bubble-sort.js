766d4485bb9aed25b876bfce7e26cad2add3880d
TechnoGeek01 <rsudharsangames@gmail.com>
2023-08-22 22:57:00 +0530

    optimized the bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,12 +1,16 @@
 function bubbleSort(arr) {
+  let noSwaps;
   for (let i = 0; i < arr.length; i++) {
+    noSwaps = true;
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        var temp = arr[j];
+        let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
